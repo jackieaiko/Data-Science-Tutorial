21221570182e4af58ef5e09aacfc15cb6fd60d2e
Jackie <jramsey@zagmail.gonzaga.edu>
2023-03-28 12:39:32 +0000

    setup

---
+++ part1.py
@@ -0,0 +1,8 @@
+from google.cloud import storage
+import os
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
---
+++ part2.py
@@ -0,0 +1,8 @@
+from google.cloud import bigquery
+import os
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
